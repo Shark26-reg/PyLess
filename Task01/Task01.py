1eb7eee4fcb1f8e7b7b047d669eed02c8f3712cb
Ivannikov Roman <shark71@inbox.ru>
2023-02-09 16:32:27 +0300

    исправил f строки в первой задаче

--- Task01/Task01.py
+++ Task01/Task01.py
@@ -12,7 +12,7 @@ seasons_list = ['Зима', 'Весна', 'Лето', 'Осень']
 seasons_dict = {1: 'Зима', 2: 'Весна', 3: 'Лето', 4: 'Осень'}
 month = int(input("Введите месяц по номеру "))
 if month == 12 or month == 1 or month == 2:
-     print(f"Результат через список: {seasons_list[0]}")
+    print(f"Результат через список: {seasons_list[0]}")
     print(f"Результат через словарь: {seasons_dict.get(1)}")
 
 elif month == 3 or month == 4 or month == 5:
